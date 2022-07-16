04f8bd0875e7e5d72d135a121a0d6ff61ec412b9
yangxinhao <yangxinhao@zhuanzhuan.com>
2022-07-16 15:02:50 +0800

    feat: 更换api地址

--- vue.config.js
+++ vue.config.js
@@ -72,7 +72,8 @@ module.exports = {
     hotOnly: false,
     proxy: {
       '/api': {
-        target: 'https://lianghj.top:8888/api/private/v1/',
+        // target: 'https://lianghj.top:8888/api/private/v1/',
+        target: 'https://hm-api.ironc.cn/api/private/v1/',
         changeOrigin: true,
         pathRewrite: {
           '^/api': ''
